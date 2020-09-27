1a06ed8ff3b9ac0ba365c96158c3998dbeab72c0
Aleksey <fduchunru@gmail.com>
2020-09-27 21:30:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(){
 }
 
 function removeLastKitten(){
-  return [kittens.slice(kittens.length-1)]
+  return [kittens.slice(kittens.length)]
 }
 
 function removeFirstKitten(){
